e62f1e3f0accdd60e3d103e9feeb1e5b0fbf530d
Anton Klimakov <anton.klimakov@office.taskdata.ru>
2021-08-30 13:00:02 +0300

    Initial commit

---
+++ nifi-HelloWorld-processors/src/main/java/ru/morkovka/processors/HelloWorld/MyProcessor.java
@@ -0,0 +1,117 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ru.morkovka.processors.HelloWorld;
+
+import org.apache.nifi.annotation.behavior.ReadsAttribute;
+import org.apache.nifi.annotation.behavior.ReadsAttributes;
+import org.apache.nifi.annotation.behavior.WritesAttribute;
+import org.apache.nifi.annotation.behavior.WritesAttributes;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.SeeAlso;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.annotation.lifecycle.OnScheduled;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.expression.ExpressionLanguageScope;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.*;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+@Tags({"example"})
+@CapabilityDescription("Prints Hello World")
+@SeeAlso({})
+@ReadsAttributes({@ReadsAttribute(attribute = "", description = "")})
+@WritesAttributes({@WritesAttribute(attribute = "", description = "")})
+public class MyProcessor extends AbstractProcessor {
+
+    public static final PropertyDescriptor nameProperty = new PropertyDescriptor
+            .Builder().name("NAME")
+            .displayName("Name")
+            .description("Name to print")
+            .required(true)
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .expressionLanguageSupported(ExpressionLanguageScope.FLOWFILE_ATTRIBUTES)
+            .build();
+
+    public static final Relationship successRel = new Relationship.Builder()
+            .name("success")
+            .description("If everything is ok")
+            .build();
+
+    public static final Relationship failureRel = new Relationship.Builder()
+            .name("failure")
+            .description("If something is wrong")
+            .build();
+
+    private List<PropertyDescriptor> descriptors;
+
+    private Set<Relationship> relationships;
+
+    @Override
+    protected void init(final ProcessorInitializationContext context) {
+        final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
+        descriptors.add(nameProperty);
+        this.descriptors = Collections.unmodifiableList(descriptors);
+
+        final Set<Relationship> relationships = new HashSet<Relationship>();
+        relationships.add(successRel);
+        relationships.add(failureRel);
+        this.relationships = Collections.unmodifiableSet(relationships);
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return this.relationships;
+    }
+
+    @Override
+    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return descriptors;
+    }
+
+    @OnScheduled
+    public void onScheduled(final ProcessContext context) {
+
+    }
+
+    @Override
+    public void onTrigger(final ProcessContext context, final ProcessSession session) throws ProcessException {
+        FlowFile flowFile = session.get();
+        if (flowFile == null) {
+            return;
+        }
+        // TODO implement
+        try {
+            String name = context.getProperty(nameProperty).evaluateAttributeExpressions(flowFile).getValue();
+
+            String result = "Hello " + name;
+            session.putAttribute(flowFile, "result", result);
+
+            try (OutputStream flowFileOutputStream = session.write(flowFile)) {
+                flowFileOutputStream.write(result.getBytes(StandardCharsets.UTF_8));
+            }
+
+            session.transfer(flowFile, successRel);
+        } catch (Exception e) {
+            session.transfer(flowFile, failureRel);
+        }
+    }
+}
